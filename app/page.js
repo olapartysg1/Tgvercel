56151e94f980a3f7d6a0dd0ebfab175575a6a6a6
olapartysg1 <olapartysg1@gmail.com>
2025-01-31 13:34:12 +0530

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "20px", textAlign: "center" }}>
+      <h1>Telegram Bot</h1>
+      <p>Your bot is running!</p>
+    </div>
+  )
+}
+
