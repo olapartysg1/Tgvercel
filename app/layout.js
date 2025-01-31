6186141b11fd2213f1dfaebb7e63e10d2ca008ab
olapartysg1 <olapartysg1@gmail.com>
2025-01-31 14:08:54 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="min-h-screen bg-white">{children}</body>
     </html>
   )
 }
